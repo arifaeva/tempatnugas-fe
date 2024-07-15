a76cd39bf979b4a764ced2c6e9ec2ee351694044
arifaeva <arifa.eva.ixa@gmail.com>
2024-07-16 02:26:29 +0700

    add button and input component

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,9 @@
 import type { Preview } from "@storybook/react";
+import "../src/styles/globals.css";
+import { Inter } from "next/font/google";
+import React from "react";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +14,7 @@ const preview: Preview = {
       },
     },
   },
+  decorators: (story) => <main className={inter.className}>{story()}</main>,
 };
 
 export default preview;
